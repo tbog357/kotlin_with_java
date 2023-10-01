7671999126988de1aac948fc8c7fbdd30cb2ede1
tbog <hahuydung357@gmail.com>
2023-10-01 23:50:00 +0700

    Ref: Init Project

---
+++ src/main/java/TestFromJava.java
@@ -0,0 +1,7 @@
+import com.test.Test;
+
+public class TestFromJava {
+  public static void main(String[] args) {
+    Test.main(args);
+  }
+}
---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,10 @@
+package com.test;
+
+class Test {
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      println("test")
+    }
+  }
+}
